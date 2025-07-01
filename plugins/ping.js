b6e1e1454e507fdc1b015332b33ce553142056ee
ArslanMD Official <numberkliye@gmail.com>
2025-07-01 18:59:01 +0500

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -1,1 +1,15 @@
 
+const ping = async (Matrix, m) => {
+    const start = new Date().getTime();
+    await Matrix.sendMessage(m.from, { text: '🏓 Pinging...' }, { quoted: m });
+    const end = new Date().getTime();
+    const pingTime = end - start;
+
+    const prefix = global.config.PREFIX || '.';
+
+    await Matrix.sendMessage(m.from, {
+        text: `*🏓 Pong!*\n\n⏱️ Response: *${pingTime}ms*\n🔖 Prefix: *${prefix}*`,
+    }, { quoted: m });
+};
+
+export default ping;
